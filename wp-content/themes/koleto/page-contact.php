7f1bc8d26ac2b318af2aae4165f5eec15a7a4bfb
Ryosuke <ryosuke.san.hana@gmail.com>
2018-07-30 14:31:39 -0700

    Added node_modules to gitignore + Contact Page

---
+++ wp-content/themes/koleto/page-contact.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Template Name: Contact
+ *
+ * @package Koleto
+ */
+
+ get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main site--stripes right">
+            <!-- Stripes for site BG -->
+            <div class="stripes primary animated fadeInDown">
+                <div class="stripes__row">
+                    <div class="stripe"></div>
+                    <div class="stripe"></div>
+                    <div class="stripe"></div>
+                    <div class="stripe"></div>
+                </div>
+                <div class="stripes__row">
+                    <div class="stripe"></div>
+                    <div class="stripe"></div>
+                    <div class="stripe"></div>
+                    <div class="stripe"></div>
+                </div>
+            </div>
+
+            <!-- SIMPLE TEXT -->
+            <section id="team" class="hero centered container not-animated">
+                <section class="segment basic padded">
+                    <h1>
+                        Effective Solutions for your Demanding Needs
+                    </h1>
+                    <p>
+                        Our dedicated team of engineers, designers and developers are here to bring you effective solutions for your demanding needs.
+                    </p>
+                </section>
+            </section>
+
+        
+            <section id="contact" class="container container--stripes not-animated">
+                <div class="stripes purple fade twist">
+                    <div class="stripes__row">
+                        <div class="stripe"></div>
+                        <div class="stripe"></div>
+                        <div class="stripe"></div>
+                        <div class="stripe"></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <section class="col-md-6">
+                        <div class="segment basic">
+                            <h2>Let’s get started</h2>
+                            <p>Don’t know where to get started? Get in contact with our elite team to see what we find best for your product and our premier solutions.</p>
+                        </div>
+                    </section>
+                    <section class="col-md-6">
+                        <div class="segment basic">
+                            <script type="text/javascript" id="aoform-script-1317fbb9-aa39-44d8-b0ad-fef19611414b:d-0001">!function(o,t,e,a){o._aoForms=o._aoForms||[],o._aoForms.push(a);var n=function(){var o=t.createElement(e);o.src=("https:"==t.location.protocol?"https://":"http://")+"mktg.kushbottles.com/acton/content/form_embed.js",o.async=!0;for(var a=t.getElementsByTagName(e)[0],n=a.parentNode,c=document.getElementsByTagName("script"),r=!1,s=0;s<c.length;s++){if(c[s].getAttribute("src")==o.getAttribute("src"))r=!0;}r?typeof(_aoFormLoader)!="undefined"?_aoFormLoader.load({id:"1317fbb9-aa39-44d8-b0ad-fef19611414b:d-0001",accountId:"29794",domain:"mktg.kushbottles.com",isTemp:false,noStyle:false,prefill:false}):"":n.insertBefore(o,a)};window.attachEvent?window.attachEvent("onload",n):window.addEventListener("load",n,!1),n()}(window,document,"script",{id:"1317fbb9-aa39-44d8-b0ad-fef19611414b",accountId:"29794",domain:"mktg.kushbottles.com",isTemp:false,noStyle:false,prefill:false});</script>
+                        </div>
+                    </section>
+                </div>
+            </section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php
+get_footer();
